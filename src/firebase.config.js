5159d69942f245661d3ab6bf5813f002273230cc
samantaavijit <samantaavijit.2000@gmail.com>
2024-02-19 16:58:10 +0530

    changes

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -2,16 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBqjl6Ya4JRwODNTJRvlx8tWeE3Kx_0j2M",
-  authDomain: "quiz-app-hs.firebaseapp.com",
-  projectId: "quiz-app-hs",
-  storageBucket: "quiz-app-hs.appspot.com",
-  messagingSenderId: "269034180324",
-  appId: "1:269034180324:web:5454805b2875534894e6f1",
-  measurementId: "G-PXP2MZZ6NL",
+  apiKey: "AIzaSyCnlRFFnROFk4f_gfQsBBwiYf8Z6w7DPFQ",
+  authDomain: "avijitsamanta-class12.firebaseapp.com",
+  projectId: "avijitsamanta-class12",
+  storageBucket: "avijitsamanta-class12.appspot.com",
+  messagingSenderId: "822484338282",
+  appId: "1:822484338282:web:86d581e6f0bd049d5c2c13",
+  measurementId: "G-4391EW9Y03",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+export default app;
